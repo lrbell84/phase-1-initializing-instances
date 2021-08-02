aea7f3628b8e5c5de56a7ce6456019f378eb9b95
Lydia Bell <lrbell84@gmail.com>
2021-08-02 02:35:53 +0000

    commit tree 24f2b3b687a20937d2f5f5cfcf9f569f3b03b31e

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here
+class Breakfast {
+    constructor(eggs, coffee) {
+        this.food = eggs
+        this.drink = coffee
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = 'side salad'
+        this.soup = 'broccoli cheddar soup'
+        this.drink = 'iced tea'
+    }
+}
+
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+      this.salad = 'balsamic salad'
+      this.soup = 'consomme'
+      this.entree = 'filet mignon'
+      this._dessert = 'cheesecake'
+    }
+}// Write your code here
